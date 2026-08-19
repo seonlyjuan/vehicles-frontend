6747a3228b92b3b89610fcb6c37c3c379d6bfbeb
JP <jean-pierrefelix.schlumpf@uzh.ch>
2026-08-19 09:19:00 +0200

    created seperate pages for bicycles, cars and motorbikes

--- src/pages/vehicles/cars/listing/ListingPage.jsx
+++ src/pages/vehicles/cars/listing/ListingPage.jsx
@@ -1,15 +1,14 @@
-// client/src/pages/listing/ListingPage.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export function ListingPage() {
+export function CarListing() {
   return (
     <div style={{ padding: '2rem' }}>
       <h1>Alle Listings</h1>
       <p>Hier siehst du eine Übersicht deiner Listings.</p>
       
       {/* Link zur Erstellungsseite */}
-      <Link to="/listing/create">
+      <Link to="/vehicles/cars/listing/create">
         <button style={{ padding: '0.5rem 1rem', cursor: 'pointer' }}>
           Inserat erstellen
         </button>
